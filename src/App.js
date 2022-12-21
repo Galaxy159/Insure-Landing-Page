a7c334042bd8c28a82f7ff65e4eca45970db5f11
Galaxy159 <braun.asaf@gmail.com>
2022-12-21 22:24:53 +0200

    init style and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./style/App.css";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Features from "./components/Features";
+import Cta from "./components/Cta";
+import Footer2 from "./components/Footer2";
 
 function App() {
   return (
     <>
       <main className="container">
-        <Footer />
+        <Navbar />
+        <Header />
+        <Features />
+        <Cta />
+        <Footer2 />
       </main>
+      <Footer />
     </>
   );
 }
