95ea247e43a00b5b8073edf047858b3d9ad6b1da
Galaxy159 <braun.asaf@gmail.com>
2023-01-09 21:10:57 +0200

    desktop

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -2,7 +2,9 @@ export default function Cta() {
   return (
     <>
       <section className="cta">
-        <h3 className="cta__headline">Find out more about how we work</h3>
+        <h3 className="cta__headline">
+          Find out more <br></br>about how we work
+        </h3>
         <button className="cta__btn">HOW WE WORK</button>
       </section>
     </>
--- src/components/Features.js
+++ src/components/Features.js
@@ -6,49 +6,50 @@ export default function Features() {
   return (
     <>
       <section className="features">
-        <div className="features__separator"></div>
         <h2 className="features__headline">We’re different</h2>
-        <div className="features__item">
-          <div className="features__item--icon">
-            <img
-              src={snappy}
-              alt="Lightning"
-              className="features__item--icon-img"
-            />
+        <div className="features__container">
+          <div className="features__item">
+            <div className="features__item--icon">
+              <img
+                src={snappy}
+                alt="Lightning"
+                className="features__item--icon-img"
+              />
+            </div>
+            <h4 className="features__item--headline">Snappy Process</h4>
+            <p className="features__item--text">
+              Our application process can be completed in minutes, not hours.
+              Don’t get stuck filling in tedious forms.
+            </p>
           </div>
-          <h4 className="features__item--headline">Snappy Process</h4>
-          <p className="features__item--text">
-            Our application process can be completed in minutes, not hours.
-            Don’t get stuck filling in tedious forms.
-          </p>
-        </div>
-        <div className="features__item">
-          <div className="features__item--icon">
-            <img
-              src={afford}
-              alt="Lightning"
-              className="features__item--icon-img"
-            />
+          <div className="features__item">
+            <div className="features__item--icon">
+              <img
+                src={afford}
+                alt="Lightning"
+                className="features__item--icon-img"
+              />
+            </div>
+            <h4 className="features__item--headline">Affordable Prices</h4>
+            <p className="features__item--text">
+              We don’t want you worrying about high monthly costs. Our prices
+              may be low, but we still offer the best coverage possible.
+            </p>
           </div>
-          <h4 className="features__item--headline">Affordable Prices</h4>
-          <p className="features__item--text">
-            We don’t want you worrying about high monthly costs. Our prices may
-            be low, but we still offer the best coverage possible.
-          </p>
-        </div>
-        <div className="features__item">
-          <div className="features__item--icon">
-            <img
-              src={people}
-              alt="Lightning"
-              className="features__item--icon-img"
-            />
+          <div className="features__item">
+            <div className="features__item--icon">
+              <img
+                src={people}
+                alt="Lightning"
+                className="features__item--icon-img"
+              />
+            </div>
+            <h4 className="features__item--headline">People First</h4>
+            <p className="features__item--text">
+              Our plans aren’t full of conditions and clauses to prevent
+              payouts. We make sure you’re covered when you need it.
+            </p>
           </div>
-          <h4 className="features__item--headline">People First</h4>
-          <p className="features__item--text">
-            Our plans aren’t full of conditions and clauses to prevent payouts.
-            We make sure you’re covered when you need it.
-          </p>
         </div>
       </section>
     </>
